447c2649257d748d5673b4502237eaf863fb6a90
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-12-01 12:43:17 +0100

    Minus i plus

--- Day1/Day1.js
+++ Day1/Day1.js
@@ -1,11 +1,11 @@
 const readFile = require("fs").readFileSync;
-const file = readFile(__dirname + "/input", "utf-8").split("\n").filter((_) => _.trim()).map((_) => parseInt(_, 10));
+const file = readFile(__dirname + "/input.txt", "utf-8").split("\n").filter((_) => _.trim()).map((_) => parseInt(_, 10));
 
 // PART 1
 let increments = 0;
 
-for (let i = 1; i <= file.length; i++) {
-    if (file[i] > file[i - 1]) increments++;
+for (let i = 0; i <= file.length; i++) {
+    if (file[i] < file[i + 1]) increments++;
 }
 
 console.log(increments);
