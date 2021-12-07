01db2ee72b9f13c5247d7332a3ff69d6c3b0ee0b
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-12-07 12:08:41 +0100

    Day 7 Advent of Code, why is this so much easier than Day 4 and 5

---
+++ Day7/Day7_2.js
@@ -0,0 +1,16 @@
+const readFile = require("fs").readFileSync;
+const file = readFile(__dirname + "/input.txt", "utf-8").split(",").filter((_) => _.trim()).map(Number);
+
+let leastFuel = 0;
+const max = Math.max(...file);
+
+for (let i = 0; i <= max; i++) {
+    let fuel = 0;
+    for (let j = 0; j < file.length; j++) {
+        let a = Math.abs(i - file[j]);
+        fuel += a * (a + 1) / 2;
+    }
+    if (fuel < leastFuel || leastFuel == 0) leastFuel = fuel;
+}
+
+console.log(leastFuel);
--- readFileTemplate.js
+++ readFileTemplate.js
@@ -1,6 +1,6 @@
 // Numbers
 const readFile = require("fs").readFileSync;
-const file = readFile(__dirname + "/input.txt", "utf-8").split("\n").filter((_) => _.trim()).map((_) => parseInt(_, 10));
+const file = readFile(__dirname + "/input.txt", "utf-8").split("\n").filter((_) => _.trim()).map(Number);
 
 // Strings
 const readFile = require("fs").readFileSync;
