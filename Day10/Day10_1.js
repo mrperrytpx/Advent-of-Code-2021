ea457beaa6e14d49209e25f8dfd90ebc45069d60
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-12-10 17:18:31 +0100

    Day 10 Advent of Code both parts, how are stacks real if our eyes aren't

---
+++ Day10/Day10_1.js
@@ -0,0 +1,26 @@
+const readFile = require("fs").readFileSync;
+const file = readFile(__dirname + "/input.txt", "utf-8").split("\n").filter((_) => _.trim()).map((_) => _.replace("\r", ""));
+
+const OPENING = ["[", "{", "(", "<"];
+const CLOSING = ["]", "}", ")", ">"];
+const SCORE = { ")": 3, "]": 57, "}": 1197, ">": 25137 };
+
+const corrupted = [];
+for (let i = 0; i < file.length; i++) {
+    let stack = [];
+    for (let j = 0; j < file[i].length; j++) {
+        if (OPENING.includes(file[i][j])) {
+            stack.push(file[i][j]);
+        } else {
+            let popped = stack.pop();
+            if (OPENING.indexOf(popped) != CLOSING.indexOf(file[i][j])) corrupted.push(file[i][j]);
+        }
+    }
+}
+
+let highscore = 0;
+for (let i = 0; i < corrupted.length; i++) {
+    highscore += SCORE[corrupted[i]];
+}
+
+console.log(highscore);
---
+++ Day10/Day10_2.js
@@ -0,0 +1,36 @@
+const readFile = require("fs").readFileSync;
+const file = readFile(__dirname + "/input.txt", "utf-8").split("\n").filter((_) => _.trim()).map((_) => _.replace("\r", ""));
+
+const OPENING = ["[", "{", "(", "<"];
+const CLOSING = ["]", "}", ")", ">"];
+const SCORE = { ")": 1, "]": 2, "}": 3, ">": 4 };
+
+let scores = [];
+for (let i = 0; i < file.length; i++) {
+    let stack = [];
+    for (let j = 0; j < file[i].length; j++) {
+        if (OPENING.includes(file[i][j])) {
+            stack.push(file[i][j]);
+        } else {
+            let popped = stack.pop();
+            if (OPENING.indexOf(popped) != CLOSING.indexOf(file[i][j])) {
+                stack = [];
+                break;
+            }
+        }
+    }
+
+    let result = 0;
+    while (stack.length != 0) {
+        let popped = stack.pop();
+        let index = OPENING.indexOf(popped);
+        let oppositeBracket = CLOSING[index];
+
+        result = result * 5 + SCORE[oppositeBracket];;
+    }
+
+    if (result > 0) scores.push(result);
+}
+scores = scores.sort((a, b) => a - b);
+let winner = scores[Math.floor(scores.length / 2)];
+console.log(winner);
