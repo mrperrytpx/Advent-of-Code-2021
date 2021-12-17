a67e94475dfc226c9ae65a0b219fd54cea4269bf
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-12-17 09:13:05 +0100

    Day 16 Advent of code Part 2, all examples work but the input doesn't, unlucky

--- Day16/Day16_2.js
+++ Day16/Day16_2.js
@@ -1,96 +1,133 @@
 const readFile = require("fs").readFileSync;
-const file = readFile(__dirname + "/input.txt", "utf-8").replace(/\r/g, "").trim();
+const file = readFile(__dirname + "/example.txt", "utf-8").replace(/\r/g, "").trim();
 
 let binaryString = "";
 for (let i = 0; i < file.length; i++) {
     binaryString += parseInt(file[i], 16).toString(2).padStart(4, 0);
 }
 
-let versionSum = 0;
-
-function newValue(type, val1, val2) {
-    let result = 0;
-    switch (type) {
-        case 0:
-            result = val1 + val2;
-            break;
-        case 1:
-            result = val1 * val2;
-            break;
-        case 2:
-            result = Math.min(val1, val2);
-            break;
-        case 3:
-            result = Math.max(val1, val2);
-            break;
-        case 5:
-            result = val1 > val2 ? 1 : 0;
-            break;
-        case 6:
-            result = val1 > val2 ? 0 : 1;
-            break;
-        case 7:
-            result = val1 === val2 ? 1 : 0;
-    }
-    return result;
-}
-
 function decodePacket(index) {
+    console.log("ENTERED!!!!");
+    console.log("Starting with index: ", index);
     const versionBits = binaryString.slice(index, index + 3);
     const version = parseInt(versionBits, 2);
-    versionSum += version;
+
+    console.log("Version number is: ", version, versionBits);
+
     const typeBits = binaryString.slice(index + 3, index + 6);
     const typeId = parseInt(typeBits, 2);
 
+    console.log("Type ID check:", typeId);
+
     index += 6;
     if (typeId === 4) {
+        console.log("Type ID is 4");
+
         let validPair = true;
+        let value = "";
 
         while (validPair !== false) {
-            const nextBit = parseInt(binaryString.charAt(index));
-
-            if (nextBit !== 1) validPair = false;
-            const bitPair = binaryString.slice(index + 1, index + 5);
+            const bitPair = binaryString.slice(index, index + 5);
+            if (bitPair.charAt(0) === "0") {
+                validPair = false;
+            }
+            console.log("Bit pair: ", bitPair);
+            if (!validPair) console.log("Stopping because it's 0");
             index += 5;
-            literalValue += bitPair;
+            value += bitPair.slice(1);
         }
-        return index;
+        console.log("VALUE BEFORE CONVERTING: ", value);
+        value = parseInt(value, 2);
+        console.log("Value of subpacket: ", value);
+        return [index, value];
 
     }
 
     if (typeId !== 4) {
+        console.log("Type ID isn't 4");
         const lengthTypeId = parseInt(binaryString.charAt(index));
+
+        console.log("Length type (0 or 1): ", lengthTypeId);
 
         if (lengthTypeId === 0) {
             index += 1;
+            console.log("Length ID is 0");
 
             const lengthInBits = binaryString.slice(index, index + 15);
             const lengthOfPackets = parseInt(lengthInBits, 2);
-            index += 15;
 
+            console.log("Next", lengthOfPackets, "is the sum of lengths of next sub packets");
+
+            index += 15;
             let shouldStopAt = index + lengthOfPackets;
+            let subpacketValues = [];
 
             while (index !== shouldStopAt) {
-                index = decodePacket(index);
-            }
+                const [newIndex, value] = decodePacket(index);
+                index = newIndex;
+                console.log("Adding ", value, "to subway");
+                subpacketValues.push(value);
 
-            return index;
+            }
+            let idk = newValue(typeId, subpacketValues);
+
+            console.log("SUBWAY:", subpacketValues, "TYPE: ", typeId, "VALUE FROM SUBWAY:", idk);
+
+            console.log("Index after recursion:", index);
+            return [index, idk];
         }
 
         if (lengthTypeId === 1) {
+            console.log("Length ID is 1");
             index += 1;
-
             const numOfPacketsInBits = binaryString.slice(index, index + 11);
             const numOfPackets = parseInt(numOfPacketsInBits, 2);
+
+            console.log("Number of subpackets following is:", numOfPackets);
             index += 11;
 
+            let subpacketValues = [];
+
             for (let i = 0; i < numOfPackets; i++) {
-                index = decodePacket(index);
+                const [newIndex, value] = decodePacket(index);
+                index = newIndex;
+                subpacketValues.push(value);
             }
+
+            let idk = newValue(typeId, subpacketValues);
+            console.log("SUBWAY:", subpacketValues, "TYPE: ", typeId, "VALUE FROM SUBWAY:", idk);
+
+            return [index, idk];
         }
     }
-    return index;
 }
 
-decodePacket(0, 0);
-console.log(versionSum);
+console.log("START__________________________________");
+console.log("Function end: ", decodePacket(0));
+
+function newValue(type, array) {
+    let result = 0;
+    switch (type) {
+        case 0:
+            result = array.reduce((a, b) => a + b, 0);
+            break;
+        case 1:
+            result = array.reduce((a, b) => a * b, 1);
+            break;
+        case 2:
+            result = Math.min(...array);
+            break;
+        case 3:
+            result = Math.max(...array);
+            break;
+        case 5:
+            result = array[0] > array[1] ? 1 : 0;
+            break;
+        case 6:
+            result = array[0] < array[1] ? 1 : 0;
+            break;
+        case 7:
+            result = array[0] === array[1] ? 1 : 0;
+    }
+    return result;
+}
